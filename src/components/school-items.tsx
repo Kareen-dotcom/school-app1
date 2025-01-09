f538b1a297c936fa0eb071ccbb2ab11df065df53
guzmankaren205 <guzmankaren205@gmail.com>
2025-01-09 19:16:29 +0000

    added sorting on click for the average GPA

--- src/components/school-items.tsx
+++ src/components/school-items.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SchoolData } from '../types/school-data'
+//import './school-items.css'
 
 type SchoolItemProps = {
     school: SchoolData
@@ -8,5 +9,13 @@ type SchoolItemProps = {
 export const SchoolItem = ({
     school
 }: SchoolItemProps) => {
-    return <li>{school.name}</li>
+    return (
+        <tr>
+            <td>{school.name}</td>
+            <td>{school.average_student_gpa}</td>
+        </tr>
+    )
+    
+    
+
 }
